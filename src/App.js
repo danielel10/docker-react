d3ccffe8ff117c200403570d41c8b2ef06b816da
Daniel Elhadad <delhadad@dalet.com>
2023-12-16 17:15:45 +0200

    init

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          as <code>src/App.js</code> dsds ddd to sadasdsadsadas.
         </p>
         <a
           className="App-link"
